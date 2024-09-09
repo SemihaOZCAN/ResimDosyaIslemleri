7152375daad05525e2ed4c645d2609ed6a9560df
Semiha OZCAN <169284658+SemihaOZCAN@users.noreply.github.com>
2024-09-09 22:33:47 +0300

    ResimDosyaIslem

---
+++ ResimDosyaIslemleri/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ResimDosyaIslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRENKSEC = new System.Windows.Forms.Button();
+            this.btnRESIMSEC = new System.Windows.Forms.Button();
+            this.btnYAZDIR = new System.Windows.Forms.Button();
+            this.btnKAYDET = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMetin = new System.Windows.Forms.TextBox();
+            this.textBoxBoyut = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRENKSEC
+            // 
+            this.btnRENKSEC.Location = new System.Drawing.Point(33, 86);
+            this.btnRENKSEC.Name = "btnRENKSEC";
+            this.btnRENKSEC.Size = new System.Drawing.Size(158, 34);
+            this.btnRENKSEC.TabIndex = 5;
+            this.btnRENKSEC.Text = "Renk Seç";
+            this.btnRENKSEC.UseVisualStyleBackColor = true;
+            this.btnRENKSEC.Click += new System.EventHandler(this.btnRENKSEC_Click);
+            // 
+            // btnRESIMSEC
+            // 
+            this.btnRESIMSEC.Location = new System.Drawing.Point(33, 37);
+            this.btnRESIMSEC.Name = "btnRESIMSEC";
+            this.btnRESIMSEC.Size = new System.Drawing.Size(158, 34);
+            this.btnRESIMSEC.TabIndex = 1;
+            this.btnRESIMSEC.Text = "Resim Seç";
+            this.btnRESIMSEC.UseVisualStyleBackColor = true;
+            this.btnRESIMSEC.Click += new System.EventHandler(this.btnRESIMSEC_Click);
+            // 
+            // btnYAZDIR
+            // 
+            this.btnYAZDIR.Location = new System.Drawing.Point(33, 135);
+            this.btnYAZDIR.Name = "btnYAZDIR";
+            this.btnYAZDIR.Size = new System.Drawing.Size(158, 34);
+            this.btnYAZDIR.TabIndex = 2;
+            this.btnYAZDIR.Text = "Yazdır";
+            this.btnYAZDIR.UseVisualStyleBackColor = true;
+            this.btnYAZDIR.Click += new System.EventHandler(this.btnYAZDIR_Click);
+            // 
+            // btnKAYDET
+            // 
+            this.btnKAYDET.Location = new System.Drawing.Point(33, 185);
+            this.btnKAYDET.Name = "btnKAYDET";
+            this.btnKAYDET.Size = new System.Drawing.Size(158, 34);
+            this.btnKAYDET.TabIndex = 3;
+            this.btnKAYDET.Text = "Kaydet";
+            this.btnKAYDET.UseVisualStyleBackColor = true;
+            this.btnKAYDET.Click += new System.EventHandler(this.btnKAYDET_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Metin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Boyut:";
+            // 
+            // textBoxMetin
+            // 
+            this.textBoxMetin.Location = new System.Drawing.Point(90, 248);
+            this.textBoxMetin.Name = "textBoxMetin";
+            this.textBoxMetin.Size = new System.Drawing.Size(101, 30);
+            this.textBoxMetin.TabIndex = 6;
+            // 
+            // textBoxBoyut
+            // 
+            this.textBoxBoyut.Location = new System.Drawing.Point(90, 295);
+            this.textBoxBoyut.Name = "textBoxBoyut";
+            this.textBoxBoyut.Size = new System.Drawing.Size(101, 30);
+            this.textBoxBoyut.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(235, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnKAYDET;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 357);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxBoyut);
+            this.Controls.Add(this.textBoxMetin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKAYDET);
+            this.Controls.Add(this.btnYAZDIR);
+            this.Controls.Add(this.btnRESIMSEC);
+            this.Controls.Add(this.btnRENKSEC);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Dosya Resim İşlemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRENKSEC;
+        private System.Windows.Forms.Button btnRESIMSEC;
+        private System.Windows.Forms.Button btnYAZDIR;
+        private System.Windows.Forms.Button btnKAYDET;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMetin;
+        private System.Windows.Forms.TextBox textBoxBoyut;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ ResimDosyaIslemleri/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResimDosyaIslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string resim;
+        Bitmap bmp;
+        Color renk;
+
+       
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRESIMSEC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+                {
+                    resim = openFileDialog1.FileName; // Seçilen resmin dosya yolunu alıyoruz
+                }
+                else
+                {
+                    MessageBox.Show("Resim seçilmedi!"); // Eğer kullanıcı iptal ederse uyarı mesajı
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message); // Hata mesajı
+            }
+        }
+
+        private void btnRENKSEC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (colorDialog1.ShowDialog(this) == DialogResult.OK)
+                {
+                    renk = colorDialog1.Color; // Seçilen rengi alıyoruz
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message); // Hata mesajı
+            }
+        }
+
+        private void btnYAZDIR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(resim)) // Eğer bir resim seçildiyse
+                {
+                    bmp = new Bitmap(resim); // Seçilen resmi Bitmap'e çeviriyoruz
+                    Graphics g = Graphics.FromImage(bmp); // Grafik nesnesi ile resmi düzenliyoruz
+
+                    // Metni belirtilen font, renk ve pozisyonla resmin üzerine yazdırıyoruz
+                    g.DrawString(textBoxMetin.Text,
+                        new Font("Segoe UI", Convert.ToUInt16(textBoxBoyut.Text), FontStyle.Bold),
+                        new SolidBrush(renk), 20, 30);
+
+                    pictureBox1.Image = bmp; // İşlenen resmi PictureBox'ta gösteriyoruz
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen önce bir resim seçin."); // Eğer resim seçilmemişse uyarı mesajı
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message); // Hata mesajı
+            }
+        }
+
+        private void btnKAYDET_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bmp != null) // Eğer düzenlenmiş bir resim varsa
+                {
+                    saveFileDialog1.Filter = "JPEG Resim|*.jpg|PNG Resim|*.png"; // Kaydetme filtreleri
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string extension = System.IO.Path.GetExtension(saveFileDialog1.FileName).ToLower(); // Dosya uzantısını alıyoruz
+                        switch (extension)
+                        {
+                            case ".jpg":
+                                bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg); // JPEG formatında kaydetme
+                                break;
+                            case ".png":
+                                bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png); // PNG formatında kaydetme
+                                break;
+                            default:
+                                MessageBox.Show("Desteklenmeyen format!"); // Eğer farklı bir format seçilirse
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kaydedilecek bir resim yok."); // Eğer resim yoksa uyarı mesajı
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message); // Hata mesajı
+            }
+        }
+    }
+}
